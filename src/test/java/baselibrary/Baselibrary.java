4317e90cf320643db76303b930e9d1a39cbbe1f4
shivamukg <shivam.dubey@ukg.com>
2022-03-18 13:33:16 +0530

    hi

---
+++ src/test/java/baselibrary/Baselibrary.java
@@ -0,0 +1,64 @@
+package baselibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Baselibrary 
+
+{	
+    public static WebDriver driver;
+	
+//    @Before
+//	public void launch ()
+//	{
+//    	System.out.println("inside beforetest");
+//    	String path = System.getProperty("user.dir");
+//        System.setProperty("webdriver.chrome.driver", path+"/src/test/resources/Drivers/chromedriver.exe");
+//		driver = new ChromeDriver();
+//		
+//		driver.manage().window().maximize();
+//		
+//		
+//	}
+//	
+//	@After
+//	
+//	public void Teardown()
+//	
+//	{System.out.println("inside after test");
+//		driver.quit();
+//	}
+//
+//	@Given("user is on website")
+//	public void user_is_on_website() 
+//	{
+//	
+//	}
+//
+//	@When("user cred")
+//	public void user_enter_valid_cred()
+//	{
+//	
+//	}
+//
+//	@And("login click")
+//	public void login_buttn_click() 
+//	{
+//	
+//	}
+//
+//	@Then("homepage is visible")
+//	public void homepage_is_visible()
+//	{
+//	
+//		}
+
+
+}
